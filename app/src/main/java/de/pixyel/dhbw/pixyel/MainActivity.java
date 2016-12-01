0568fe1ed579dd28b58d4e7a77d619afdbbc9ce7
BlackReaper11 <dan-pierre.drehlich@gmx.de>
2016-12-01 13:34:24 +0100

    Galerie Zugriff funst

--- app/src/main/java/de/pixyel/dhbw/pixyel/MainActivity.java
+++ app/src/main/java/de/pixyel/dhbw/pixyel/MainActivity.java
@@ -133,7 +133,6 @@ public class MainActivity extends AppCompatActivity{
         });
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Check which request we're responding to
