30e7bd82f97caff762179716a257850b4249885b
Stefan Schmid <i15026@hb.dhbw-stuttgart.de>
2016-12-08 13:43:25 +0100

    Abstürze sind behoben

--- app/src/main/java/de/pixyel/dhbw/pixyel/Help.java
+++ app/src/main/java/de/pixyel/dhbw/pixyel/Help.java
@@ -1,14 +1,14 @@
 package de.pixyel.dhbw.pixyel;
 
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 public class Help extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
     }
+
 }
